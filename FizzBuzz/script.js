0dc3dccd90c4fd0cd2429acec314b3abecfb1ae5
Abdulrehmanansari7 <abdulrehman49622@gmail.com>
2026-04-10 18:48:04 +0530

    output appears on screen instead of console

--- FizzBuzz/script.js
+++ FizzBuzz/script.js
@@ -1,17 +1,23 @@
-function start(){
-    const num = document.querySelector("input").value;
-    for (let i = 1; i <= num; i++){
-        if(i % 3 === 0 && i % 5 === 0){
-            console.log("FizzBuzz");
-        } else if(i % 5 === 0){
-            console.log("Buzz");
-        } else if(i % 3 === 0){
-            console.log("Fizz");
-        } else{
-            console.log(i);
+    function start(){
+        const num = document.querySelector("input").value;
+        const output = document.querySelector(".output");
+        output.style.display = "flex"; 
+
+        output.innerHTML = "";
+        for (let i = 1; i <= num; i++){
+            if(i % 3 === 0 && i % 5 === 0){
+                output.innerHTML += "FizzBuzz "
+            } else if(i % 5 === 0){
+                output.innerHTML += "(Buzz) "
+            } else if(i % 3 === 0){
+                output.innerHTML += "(Fizz) "
+            } else{
+                output.innerHTML += `${i} `;
+            } if(i % 5 === 0){
+                output.innerHTML += "<br>"
+            }
         }
     }
-}
 
 
 
